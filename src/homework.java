0c6de62cd7b6087a81fe3a1ff8470d478da9c840
Carlos Penaranda <cpenara1@gmail.com>
2022-07-07 21:07:59 -0500

    code to operate with diferent numbers

--- src/homework.java
+++ src/homework.java
@@ -1,5 +1,31 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class homework {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args){
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Please enter the oper: ");
+            String oper = sc.next();
+            System.out.println("Please enter first number: ");
+            Double num1 = sc.nextDouble();
+            System.out.println("Please enter second number: ");
+            Double num2 = sc.nextDouble();
+            Double result;
+            if (Objects.equals(oper, "-")){
+                result = num1 - num2;
+                System.out.println("the result is " + result);
+            }else if (Objects.equals(oper, "+")){
+                result = num1 + num2;
+                System.out.println("the result is " + result);
+            }else if (Objects.equals(oper, "/")){
+                result = num1 / num2;
+                System.out.println("the result is " + result);
+            }else if (Objects.equals(oper, "*")){
+                result = num1 * num2;
+                System.out.println("the result is " + result);
+            }else{
+                System.out.println("invalid oper: " + oper);
+            }
+        }
     }
 }
